cb71fba70a7c2fe49a8852e88cc980cbee486bbd
Dariusz Majnert <dariusz.majnert@gmail.com>
2022-01-31 12:50:44 +0100

    tweak restart handler to ensure response

--- main/rest/device/restart.c
+++ main/rest/device/restart.c
@@ -1,15 +1,20 @@
 #include <esp_http_server.h>
 #include <cJSON.h>
 #include "rest_utils.h"
-
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 cJSON *device_restart(httpd_req_t *req)
 {
     cJSON *root = cJSON_CreateObject();
     cJSON_AddBoolToObject(root, "ok", true);
 
-    // make sure the response was sent properly
-    if (respond_json(req, root) == ESP_OK)
+    int sockfd = httpd_req_to_sockfd(req);
+
+    // make sure the response was sent properly and close the socket
+    if (respond_json(req, root) == ESP_OK && httpd_sess_trigger_close(req->handle, sockfd) == ESP_OK)
     {
+        // wait a second before restarting
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
         esp_restart();
     }
 
